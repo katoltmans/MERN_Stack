d5a5ed60b0dded16a01ded181f172d1a901ec289
katoltmans <katoltmans@msn.com>
2022-04-09 17:29:47 -0400

    Initial commit

---
+++ react/intro_to_react/first_jsx/src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Hello Dojo!</h1>
+                <p>
+                    <strong>Things I need to do:</strong>
+                </p>
+                <ul>
+                    <li>Learn React</li>
+                    <li>Climb Mt. Everest</li>
+                    <li>Run a marathon</li>
+                    <li>Feed the dogs</li>
+                </ul>
+            </header>
+        </div>
+    );
+}
+
+export default App;
