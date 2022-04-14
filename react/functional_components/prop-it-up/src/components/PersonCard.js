1c50bf9e15fde70f06e9d5fb8c290a6bfd4b685c
katoltmans <katoltmans@msn.com>
2022-04-13 21:35:55 -0400

    Add birthday button and styling

--- react/functional_components/prop-it-up/src/components/PersonCard.js
+++ react/functional_components/prop-it-up/src/components/PersonCard.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PersonCard = (props) => {
     const { person } = props;
-    console.log(person);
+    console.log("person: ", person);
+    const { firstName, lastName, age, hairColor } = person;
+    const [personAge, setPersonAge] = useState(age);
+
+    const handleClick = () => {
+        console.log(personAge);
+        setPersonAge(personAge + 1);
+    };
     return (
         <div>
             <h2>
                 {person.lastName}, {person.firstName}
             </h2>
-            <p>Age: {person.age}</p>
+            <p>Age: {personAge}</p>
             <p>Hair Color: {person.hairColor}</p>
+            <button onClick={handleClick}>
+                Birthday Button for {person.firstName} {person.lastName}
+            </button>
         </div>
     );
 };
