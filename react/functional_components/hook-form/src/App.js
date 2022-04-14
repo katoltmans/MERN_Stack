479562fb9bd797fb22707095c11473e135a7c105
katoltmans <katoltmans@msn.com>
2022-04-14 19:55:03 -0400

    Add form fields and display in real time

--- react/functional_components/hook-form/src/App.js
+++ react/functional_components/hook-form/src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Form from "./components/form.jsx";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <div className="App">
+            <Form />
+        </div>
+    );
 }
 
 export default App;
--- react/functional_components/hook-form/src/components/form.jsx
+++ react/functional_components/hook-form/src/components/form.jsx
@@ -12,7 +12,7 @@ const Form = (props) => {
         //prevent data from bring lost on page load
         e.preventDefault();
         //keys will match variable names
-        const newUser = { username, email, password };
+        const newUser = { firstName, lastName, email, password };
         console.log(newUser);
         //save state values for created user
         setFirstName("");
@@ -25,14 +25,59 @@ const Form = (props) => {
     return (
         //create form
         <form onSubmit={createUser}>
-            <div>
-                <label>First Name: </label>
+            <div className="inputField">
+                <label className="flex1">First Name: </label>
                 <input
+                    className="flex1"
                     type="text"
                     value={firstName}
                     onChange={(e) => setFirstName(e.target.value)}
                 />
             </div>
+            <div className="inputField">
+                <label className="flex1">Last Name: </label>
+                <input
+                    className="flex1"
+                    type="text"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                />
+            </div>
+            <div className="inputField">
+                <label className="flex1">Email: </label>
+                <input
+                    className="flex1"
+                    type="text"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </div>
+            <div className="inputField">
+                <label className="flex1">Password: </label>
+                <input
+                    className="flex1"
+                    type="text"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </div>
+            <div className="inputField">
+                <label>Confirm Password: </label>
+                <input
+                    type="text"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+            </div>
+            <input type="submit" value="submit"></input>
+            <h5>Your Form Data:</h5>
+            <p>First Name: {firstName}</p>
+            <p>Last Name: {lastName}</p>
+            <p>Email: {email}</p>
+            <p>Password: {password}</p>
+            <p>Confirm Password: {confirmPassword}</p>
         </form>
     );
 };
+
+export default Form;
