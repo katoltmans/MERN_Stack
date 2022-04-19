f49a2c667623367e17ff354fd6f034370d7ca937
katoltmans <katoltmans@msn.com>
2022-04-18 21:57:29 -0400

    initial commit

---
+++ react/functional_components/box-generator/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import ChangeColor from "./components/AddColorComponent.js";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header"></header>
+        </div>
+    );
+}
+
+export default App;
---
+++ react/functional_components/box-generator/src/components/AddColorComponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ChangeColor() {
+    return (
+        <div>
+            <h1>Hi</h1>
+        </div>
+    );
+}
