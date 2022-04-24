1c0af0b0d6b76b98e1efce682fe6830cf218c875
katoltmans <katoltmans@msn.com>
2022-04-23 23:58:30 -0400

    Begin TodoForm

--- react/functional_components/todo-list/src/components/Form.js
+++ react/functional_components/todo-list/src/components/Form.js
@@ -1,2 +1,9 @@
 import React, { useState } from "react";
 import { v4 } from "uuid";
+
+function TodoForm(props) {
+    const { todoItems, setTodoItems } = props;
+    const [todo, setTodo] = useState("");
+    const [seen, setSeen] = useState("false");
+    const [error, setError] = useState("");
+}
