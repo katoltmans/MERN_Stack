99050012569e2d1436f713517450f9af23dd787e
katoltmans <katoltmans@msn.com>
2022-04-24 21:18:16 -0400

    Add Form.module.css

--- react/functional_components/todo-list/src/App.js
+++ react/functional_components/todo-list/src/App.js
@@ -5,35 +5,35 @@ import background from "./img/marvel_phase_4.jpg";
 import { useState } from "react";
 
 function App() {
-    const items = [
-        {
-            id: 1,
-            todo: "Finish MoonNight series",
-            seen: false,
-        },
-        {
-            id: 2,
-            todo: "Watch Doctor Strange in the Multiverse of Madness (May 6)",
-            seen: false,
-        },
-        {
-            id: 3,
-            todo: "Begin Ms. Marvel series (June 8)",
-            seen: false,
-        },
-        {
-            id: 4,
-            todo: "Go see Thor: Love and Thunder (July 8)",
-            seen: false,
-        },
-        {
-            id: 5,
-            todo: "Anxiously await Black Panther: Wakanda Forever (November 11)",
-            seen: false,
-        },
-    ];
+    // const items = [
+    //     {
+    //         id: 1,
+    //         todo: "Finish MoonNight series",
+    //         seen: false,
+    //     },
+    //     {
+    //         id: 2,
+    //         todo: "Watch Doctor Strange in the Multiverse of Madness (May 6)",
+    //         seen: false,
+    //     },
+    //     {
+    //         id: 3,
+    //         todo: "Begin Ms. Marvel series (June 8)",
+    //         seen: false,
+    //     },
+    //     {
+    //         id: 4,
+    //         todo: "Go see Thor: Love and Thunder (July 8)",
+    //         seen: false,
+    //     },
+    //     {
+    //         id: 5,
+    //         todo: "Anxiously await Black Panther: Wakanda Forever (November 11)",
+    //         seen: false,
+    //     },
+    // ];
 
-    const [todoItems, setTodoItems] = useState(items);
+    const [todoItems, setTodoItems] = useState([]);
 
     return (
         <div className="App" style={{ backgroundImage: `url(${background})` }}>
--- react/functional_components/todo-list/src/components/Form.js
+++ react/functional_components/todo-list/src/components/Form.js
@@ -1,29 +1,28 @@
 import React, { useState } from "react";
+import styles from "./Form.module.css";
 import { v4 } from "uuid";
 
 function TodoForm(props) {
     const { todoItems, setTodoItems } = props;
     const [todo, setTodo] = useState("");
-    const [seen, setSeen] = useState("false");
     const [error, setError] = useState("");
 
     //handle submit of form
     const handleSubmit = (e) => {
         e.preventDefault();
         if (todo) {
-            console.log("todo entered", todo, seen, v4());
+            console.log("todo entered", todo, v4());
 
             //define a todo object
             const todoItem = {
                 id: v4(),
                 todo: todo,
-                seen: seen,
+                seen: false,
             };
             //create a todo item and set error
             setTodoItems([...todoItems, todoItem]);
             e.target.reset();
             setTodo("");
-            setSeen("true");
         } else {
             setError("Please enter a Marvel title to the list.");
         }
@@ -36,9 +35,10 @@ function TodoForm(props) {
             {/* handle onSubmit */}
 
             {/* form */}
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Add Marvel Media To List:</label>
                 <input
+                    className={styles.input}
                     type="text"
                     placeholder="Enter a Marvel movie or series to view."
                     value={todo}
