add4bd32419f5e67d4d887658d38d48e7efeec39
katoltmans <katoltmans@msn.com>
2022-04-26 19:29:46 -0400

    Add DisplayNames component - need to figure out how to display all names

---
+++ react/functional_components/pokemon-api/src/components/PokemonNames.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+
+const DisplayNames = (props) => {
+    const [names, setNames] = useState(0);
+
+    useEffect(() => {
+        console.log("Names");
+        fetch("https://pokeapi.co/api/v2/pokemon/")
+            .then((response) => {
+                return response.json();
+            })
+            .then((response) => {
+                setNames({
+                    name: response.results,
+                });
+            });
+    }, []);
+
+    return (
+        <div>
+            {names.name
+                ? names.name.map((item, index) => {
+                      return <div key={index}>{item.name}</div>;
+                  })
+                : null}
+        </div>
+    );
+};
+
+export default DisplayNames;
