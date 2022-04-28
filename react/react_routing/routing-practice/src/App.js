4e6270d5e22678a9be8259865358ca8b68dc0d44
katoltmans <katoltmans@msn.com>
2022-04-28 15:55:53 -0400

    Add Home component

--- react/react_routing/routing-practice/src/App.js
+++ react/react_routing/routing-practice/src/App.js
@@ -1,12 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home.js";
 
 function App() {
     return (
         <BrowserRouter>
             <div className="App">
-                <Routes></Routes>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
             </div>
         </BrowserRouter>
     );
---
+++ react/react_routing/routing-practice/src/components/Home.js
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h1>Hello</h1>
+        </div>
+    );
+};
+
+export default Home;
