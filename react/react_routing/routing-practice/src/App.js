cdb635c080afb1fd58f088cafbacd999b2469b37
katoltmans <katoltmans@msn.com>
2022-04-28 14:43:36 -0400

    Initial commit

---
+++ react/react_routing/routing-practice/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Routes></Routes>
+            </div>
+        </BrowserRouter>
+    );
+}
+
+export default App;
