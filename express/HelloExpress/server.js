7a0deca99b898083ebd90f014cbd3ef07a20c85f
katoltmans <katoltmans@msn.com>
2022-05-01 17:07:19 -0400

    install express and change string value to json response method

--- express/HelloExpress/server.js
+++ express/HelloExpress/server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
+const port = 8000;
 
 // req is short for request
 // res is short for response
 app.get("/api", (req, res) => {
-  res.send("Our express api server is now sending this over to the browser");
+    //res.json("Our express api server is now sending this over to the browser");
+    res.json({ message: "Hello World!!" });
 });
 
 const server = app.listen(8000, () =>
-  console.log(`Server is locked and loaded on port ${server.address().port}!`)
+    console.log(`Server is locked and loaded on port ${server.address().port}!`)
 );
+
+//app.listen(port, () => console.log(`Listening on port: ${port}`));
