13fe1d24026d2876efa85e886dab46ee4f1831ea
katoltmans <katoltmans@msn.com>
2022-05-05 20:24:02 -0400

    Font-end and back-end connected!

--- full_stack_MERN/full_stack/myNewProject/server/server.js
+++ full_stack_MERN/full_stack/myNewProject/server/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+app.use(cors());
 const PORT = 8000;
+
 require("./routes/person.routes")(app); // We're importing the routes export.
 // These two lines are the long-hand notation of the code above. They better show what's going on.
 // const personRoutes = require("./routes/person.routes");  <-- assign the exported function to a const
