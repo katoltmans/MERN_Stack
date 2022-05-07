cc69aa654eb39f8da34c4b1831981c9ac7804dc2
katoltmans <katoltmans@msn.com>
2022-05-07 15:08:32 -0400

    Front end and back end talking to one another

--- full_stack_MERN/full_stack/productManager/server/server.js
+++ full_stack_MERN/full_stack/productManager/server/server.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const cors = require("cors"); //allows servers to interact with each other
 const app = express();
+app.use(cors()); //use cors
 const PORT = 8000;
+
+// body parser to fix body issues
+app.use(express.json(), express.urlencoded({ extended: true }));
+
+const AllProductRoutes = require("./routes/product.routes");
+AllProductRoutes(app);
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
