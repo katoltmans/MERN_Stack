ef029d7bc3fd8ca8957605fb145a163210ac5917
katoltmans <katoltmans@msn.com>
2022-05-16 14:00:08 -0400

    Material-UI example initial commit

---
+++ full_stack_MERN/full_stack/material-ui-example/src/components/LoginForm.js
@@ -0,0 +1,52 @@
+import React from "react";
+import {
+    Paper,
+    FormControl,
+    InputLabel,
+    OutlinedInput,
+    Button,
+} from "@material-ui/core";
+
+const styles = {
+    paper: {
+        width: "20rem",
+        padding: "1rem",
+    },
+    input: {
+        marginBottom: "1rem",
+    },
+    button: {
+        width: "100%",
+    },
+};
+
+const LoginForm = () => {
+    return (
+        <Paper elevation={3} style={styles.input}>
+            <h2>Login Form</h2>
+            <form>
+                <FormControl variant="outlined" style={styles.input}>
+                    <InputLabel>UserName</InputLabel>
+                    <OutlinedInput type="text" />
+                </FormControl>
+                <FormControl variant="outlined" style={styles.input}>
+                    <InputLabel>E-mail</InputLabel>
+                    <OutlinedInput type="email" />
+                </FormControl>
+                <FormControl variant="outlined" style={styles.input}>
+                    <InputLabel>Password</InputLabel>
+                    <OutlinedInput type="password" />
+                </FormControl>
+                <FormControl variant="outlined" style={styles.input}>
+                    <InputLabel>Confirm Password</InputLabel>
+                    <OutlinedInput type="password" />
+                </FormControl>
+                <Button type="submit" variant="contained" color="primary">
+                    Register
+                </Button>
+            </form>
+        </Paper>
+    );
+};
+
+export default LoginForm;
