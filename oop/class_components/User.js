1dc6f71b0d8e04da172260dc218fbb3592b71b4c
katoltmans <katoltmans@msn.com>
2022-05-23 12:46:48 -0400

    Add User class demo (bank account)

--- oop/class_components/User.js
+++ oop/class_components/User.js
@@ -10,4 +10,58 @@ class User {
     makeDeposit(amount) {
         this.accountBalance += amount;
     }
+
+    // Method for making withdrawals
+    makeWithdrawal(amount) {
+        this.accountBalance -= amount;
+    }
+
+    // Method to display a user's account balance
+    displayBalance() {
+        console.log(
+            "User: " +
+                this.name +
+                ", Balance: $" +
+                this.accountBalance.toFixed(2)
+        );
+    }
+
+    // Method to transfer money
+    transferMoney(amount, receiver) {
+        this.accountBalance -= amount;
+        receiver.accountBalance += amount;
+        console.log(
+            this.name +
+                "'s Balance: $" +
+                this.accountBalance +
+                ", " +
+                receiver.name +
+                "'s Balance: $" +
+                receiver.accountBalance
+        );
+    }
 }
+
+const jake = new User("Jake Peralta", "jperalta@99.com");
+const amy = new User("Amy Santiago", "asantiago@99.com");
+const rosa = new User("Rosa Diaz", "rdiaz@99.com");
+
+jake.makeDeposit(10);
+jake.makeDeposit(100);
+jake.makeDeposit(23.5);
+jake.makeWithdrawal(115);
+jake.displayBalance();
+
+amy.makeDeposit(500);
+amy.makeDeposit(975);
+amy.makeWithdrawal(220);
+amy.makeWithdrawal(13.78);
+amy.displayBalance();
+
+rosa.makeDeposit(500);
+rosa.makeWithdrawal(225);
+rosa.makeWithdrawal(215);
+rosa.makeWithdrawal(65.89);
+rosa.displayBalance();
+
+amy.transferMoney(100, rosa);
