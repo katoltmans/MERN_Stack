4c13b071e42cc5e3b56c12efea591a6568d7a4a1
katoltmans <katoltmans@msn.com>
2022-05-24 21:23:00 -0400

    Add PersonCard component

--- oop/class-components/big-inversion/src/App.js
+++ oop/class-components/big-inversion/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import PersonCard from "./components/PersonCard";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <PersonCard
+                lastName={"Peralta"}
+                firstName={"Jake"}
+                age={33}
+                hairColor={"brown"}
+            />
+            <PersonCard
+                lastName={"Santiago"}
+                firstName={"Amy"}
+                age={32}
+                hairColor={"black"}
+            />
+            <PersonCard
+                lastName={"Jeffords"}
+                firstName={"Terry"}
+                age={35}
+                hairColor={"n/a"}
+            />
+            <PersonCard
+                lastName={"Diaz"}
+                firstName={"Rosa"}
+                age={33}
+                hairColor={"black"}
+            />
+        </div>
+    );
 }
 
 export default App;
