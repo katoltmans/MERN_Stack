e23f765f05280f182ffde64c34f53ce942cf0180
katoltmans <katoltmans@msn.com>
2022-05-25 19:40:16 -0400

    Add birthday button

--- oop/class-components/big-inversion/src/components/PersonCard.js
+++ oop/class-components/big-inversion/src/components/PersonCard.js
@@ -3,7 +3,16 @@ import React, { Component } from "react";
 class PersonCard extends Component {
     constructor(props) {
         super(props);
+        this.state = { age: props.age };
     }
+
+    // Method to increase person's age when the birthday button is clicked
+    increaseAge = () => {
+        console.log(this.state.age);
+        this.setState((state, props) => ({
+            age: state.age + 1,
+        }));
+    };
 
     render() {
         const { firstName, lastName, age, hairColor } = this.props;
@@ -13,8 +22,11 @@ class PersonCard extends Component {
                 <h2>
                     {lastName}, {firstName}
                 </h2>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {hairColor}</p>
+                <button onClick={this.increaseAge}>
+                    Birthday Button for {firstName} {lastName}
+                </button>
             </div>
         );
     }
